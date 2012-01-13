6a71c7be1f6dd3b9c127f580661bf6d4876cfee1
Aidy Lewis <aidy.lewis@googlemail.com>
2012-01-13 12:23:50 +0000

    included internal server error logging

--- lib/tsung_log_parser.rb
+++ lib/tsung_log_parser.rb
@@ -1,9 +1,12 @@
 class TsungLogParser
-  def self.execute output, tsung_log
+  def self.execute output, tsung_log, tsung_dump
     parsed_log = LogParser.parse(File.read tsung_log)
     output.puts "Global Mean: #{parsed_log[:global_mean]}"
     output.puts "Minimum Response Time: #{parsed_log[:min]}"
     output.puts "Maximum Response Time: #{parsed_log[:max]}"
     output.puts "Total User Count: #{parsed_log[:finish_users_count]}"
+    parsed_dump = DumpParser.parse(File.read tsung_dump)
+    output.puts "Internal Server Errors (500's) : #{parsed_dump[:internal_server_errors]}"
+    p parsed_log
   end
 end
--- test/tsung_log_parser_test.rb
+++ test/tsung_log_parser_test.rb
@@ -16,9 +16,11 @@ class TsungLogParserTest <  MiniTest::Unit::TestCase
     end
   end
 
-  def test_writes_all_log_data
-    file = Tempfile.new("tsung.log").path
-    File.open(file, "w") do |file|
+  def setup
+    @log_file = Tempfile.new("tsung.log").path
+    @dump_file = Tempfile.new("tsung.dump").path
+
+    File.open(@log_file, "w") do |file|
       file.write <<-LOG
       {
          "stats" : [
@@ -42,11 +44,29 @@ class TsungLogParserTest <  MiniTest::Unit::TestCase
       LOG
     end
 
+    File.open(@dump_file, "w") do |file|
+      file.write <<-DUMP
+        Recv:1326372146.48497:<0.104.0>:HTTP/1.1 500 Internal Server Error
+        X-Frame-Options: sameorigin
+
+        Recv:1326372146.48497:<0.104.0>:HTTP/1.1 500 Internal Server Error
+        X-Frame-Options: sameorigin
+      DUMP
+    end
+  end
+
+  def test_writes_all_log_data
     output = OutputSpy.new
-    TsungLogParser.execute(output, file)
+    TsungLogParser.execute(output, @log_file, @dump_file)
     output.output.must_include "Global Mean: 25634.239"
     output.output.must_include "Minimum Response Time: 5555.1234"
     output.output.must_include "Maximum Response Time: 9999.9999"
     output.output.must_include "Total User Count: 430"
+  end
+
+  def test_writes_dump_data
+    output = OutputSpy.new
+    TsungLogParser.execute(output, @log_file, @dump_file)
+    output.output.must_include "Internal Server Errors (500's) : 2"
   end
 end
